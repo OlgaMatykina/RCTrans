1d3b0f64b5a8ed0abcac037d5e135888cc0f0f28
OlgaMatykina <omatykina@mail.ru>
2025-04-02 15:05:55 +0300

    fit training sbd to cds2

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-docker run --rm -it -d --shm-size=64gb --gpus all \
-    -v /media/matykina_ov/FastSSD/RCTrans:/home/docker_rctrans/RCTrans \
-    -v /media/matykina_ov/HPR1:/home/docker_rctrans/HPR1 \
-    -v /media/matykina_ov/data:/home/docker_rctrans/data \
-    -v /home/matykina_ov:/home/docker_rctrans/HPR2 \
-    -v /media/matykina_ov/FastSSD:/home/docker_rctrans/HPR3 \
-    --name matykina_rctrans_hpr3  rctrans:latest "/bin/bash"
+docker run --rm -it -d --shm-size=64gb --gpus '"device=1"' \
+    -v /home/matykina_ov/RCTrans:/home/docker_rctrans/RCTrans \
+    -v /datasets:/home/docker_rctrans/HPR1 \
+    --name matykina_rctrans  rctrans:latest "/bin/bash"
