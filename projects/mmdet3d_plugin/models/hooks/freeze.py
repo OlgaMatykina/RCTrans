eafb190bb8b1136698de081f32b2a18bd897da84
OlgaMatykina <omatykina@mail.ru>
2025-04-24 00:04:17 +0300

    pts head unfreezed

--- projects/mmdet3d_plugin/models/hooks/freeze.py
+++ projects/mmdet3d_plugin/models/hooks/freeze.py
@@ -6,7 +6,7 @@ class FreezeAllButNewDepthHook(Hook):
     def before_train(self, runner):
         model = runner.model.module if hasattr(runner.model, 'module') else runner.model
         for name, param in model.named_parameters():
-            if 'depth_model' in name or 'radar_depth_model' in name:
+            if 'depth_model' in name or 'radar_depth_model' in name or 'pts_bbox_head' in name:
                 param.requires_grad = True
             else:
                 param.requires_grad = False
